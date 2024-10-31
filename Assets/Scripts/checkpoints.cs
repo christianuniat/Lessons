771eef5ed5f929abdef626b0c8074639737500e0
Magodelvacio <abraham.bustos@uniat.edu.mx>
2024-10-31 13:16:25 -0600

    check point

---
+++ Assets/Scripts/checkpoints.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class checkpoints : MonoBehaviour
+{
+    public Transform checkpoint;
+    public List<Transform>transforms;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/movimientoSideScroll.cs
+++ Assets/Scripts/movimientoSideScroll.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,13 +11,18 @@ public class movimientoSideScroll : MonoBehaviour
     float movX;
     Rigidbody2D rbtopdown;
     Animator anim;
-    int jumps=2;
-    
+    int jumps = 2;
+
+    public checkpoints checkpoint;
+    public int savedCheckpoint;
     // Start is called before the first frame update
     void Start()
     {
        rbtopdown = GetComponent<Rigidbody2D>();
        anim = GetComponent<Animator>();
+       transform.position = checkpoint.checkpoint.position;
+        //esto es lo mismo pero para el sistema de guardado.
+       transform.position = checkpoint.transforms[savedCheckpoint].position;
     }
 
     // Update is called once per frame
@@ -45,6 +51,12 @@ public class movimientoSideScroll : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Muerte")) transform.position = Vector2.zero;
+        if (other.gameObject.CompareTag("Muerte")) transform.position = checkpoint.transforms[savedCheckpoint].position;
+        if (other.gameObject.CompareTag("Checkpoint")) 
+        {
+            checkpoint.checkpoint.position = other.transform.position;
+            other.enabled = false;
+            savedCheckpoint++;
+        }
     }
 }
